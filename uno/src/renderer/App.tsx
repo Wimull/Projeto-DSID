f0c916d24d40d462e1ab9fc6562da082866d4de3
Ana Queiroz da Silva <aninhasilva@tutanota.com>
2026-07-10 18:34:13 -0300

    Added images

--- uno/src/renderer/App.tsx
+++ uno/src/renderer/App.tsx
@@ -1,4 +1,7 @@
 import React from "react"
+// Suppress TypeScript error for side-effect CSS import when no declaration is present
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore: TS2307
 import "./index.css"
 
 export default function App() {
